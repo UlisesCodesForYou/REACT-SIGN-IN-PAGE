7862180b3a373c5bbe4239cd260e3b6dde853747
UlisesCodesForYou <76757797+UlisesCodesForYou@users.noreply.github.com>
2022-04-24 21:15:08 -0700

    Added auto logout and optimized token data.

--- src/components/store/auth-context.js
+++ src/components/store/auth-context.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+let logoutTimer; 
+
 const AuthContext = React.createContext({
   token: "",
   isLoggedIn: false,
@@ -7,17 +9,59 @@ const AuthContext = React.createContext({
   logOut: () => {},
 });
 
+// #### HELPER FUNCTION TO CALCULATE LOGOUT TIME ######
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime()
+  const adjExpirationTime = new Date(expirationTime)
+
+  const remainingExpirationTime = adjExpirationTime - currentTime;
+  
+  return remainingExpirationTime;
+}
+
+const retrieveStoredToken = ()=> {
+  const storedToken = localStorage.getItem('token');
+  const storedExpirationDate = localStorage.getItem('expirationTime')
+  const remainingTime = calculateRemainingTime(storedExpirationDate)
+  
+  if(remainingTime <= 3600) {
+    localStorage.removeItem('token')
+    localStorage.removeItem('expirationTime')
+    return null
+  }
+
+  return {
+    token: storedToken,
+    time: remainingTime
+  }
+}
+// #### END OF HELPER FUNCTION ########
+
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const  tokenData = retrieveStoredToken
+  const initialToken = tokenData.token;  //This checks if there's a token in local storage before the user stores one.
+  const [token, setToken] = useState(initialToken);
+
+
 
   const userIsLoggedIn = !!token;
 
-  const loggedInHandler = (token) => {
-    setToken(token);
+  const loggedOutHandler = () => {
+    localStorage.removeItem('token') //Removes token from local storage. 
+    setToken(null);
+    if (logoutTimer) {
+      clearTimeout(logoutTimer)
+    }
   };
 
-  const loggedOutHandler = () => {
-    setToken(null);
+  const loggedInHandler = (token, expirationTime) => {
+    localStorage.setItem("token", token); //Storing a token in local storage.
+    setToken(token);
+    localStorage.setItem('expirationTime', expirationTime)
+
+    const remainingTokenTime = calculateRemainingTime(expirationTime)
+
+    logoutTimer = setTimeout(loggedOutHandler, remainingTokenTime)
   };
 
   const contextValue = {
